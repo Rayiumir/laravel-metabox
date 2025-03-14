e0f8e9369561c04731ae4ad2fb49ad0e9dfddaeb
Rayiumir <swordart2050@gmail.com>
2025-03-14 04:12:47 +0330

    Created Traits Metabox

---
+++ src/Traits/HasMetaboxes.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Rayiumir\LaravelMetabox\Traits;
+
+use Rayiumir\LaravelMetabox\Models\Metabox;
+
+trait HasMetaboxes
+{
+    public function metaboxes()
+    {
+        return $this->morphMany(Metabox::class, 'model');
+    }
+
+    public function addMetabox($key, $value)
+    {
+        return $this->metaboxes()->updateOrCreate(
+            ['key' => $key],
+            ['value' => $value]
+        );
+    }
+
+    public function getMetabox($key)
+    {
+        $metabox = $this->metaboxes()->where('key', $key)->first();
+        return $metabox ? $metabox->value : null;
+    }
+
+    public function removeMetabox($key)
+    {
+        return $this->metaboxes()->where('key', $key)->delete();
+    }
+}
